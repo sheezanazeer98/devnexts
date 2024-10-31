f5cecdc74e0e81655afa42ffa370f9e9f7c9e4a0
Sheeza Nazeer <sheezanazeer98@gmail.com>
2024-11-01 03:29:01 +0500

    first commit

---
+++ src/utils/data.js
@@ -0,0 +1,146 @@
+export const HeroData = [
+  {
+    src: "hero/person1.jpg",
+    bg: "#0be4d1",
+    delay: 0.1,
+  },
+  {
+    src: "hero/person2.jpg",
+    bg: "#fde490",
+    delay: 0.3,
+  },
+  {
+    src: "hero/person3.jpg",
+    bg: "#00c9f7",
+    delay: 0.2,
+  },
+  {
+    src: "hero/person4.jpg",
+    bg: "#83cfdf",
+    delay: 0.2,
+  },
+  {
+    src: "hero/person5.jpg",
+    bg: "#fe8856",
+    delay: 0.3,
+  },
+  {
+    src: "hero/person6.jpg",
+    bg: "#0be4d1",
+    delay: 0.25,
+  },
+];
+
+export const features = [
+  {
+    icon: "/features/Frame-0.png",
+    title: "3x monthly revenue",
+  },
+  {
+    icon: "/features/Frame-1.png",
+    title: "Flat fee of 6-12%",
+  },
+  {
+    icon: "/features/Frame-2.png",
+    title: "Flexible repayments by revenue share",
+  },
+  {
+    icon: "/features/Frame-3.png",
+    title: "3-4 years repayment",
+  },
+  {
+    icon: "/features/Frame-4.png",
+    title: "$10-100k seed investment",
+  },
+  {
+    icon: "/features/Frame-5.png",
+    title: "2% of revenue up to 3X cap",
+  },
+];
+
+export const ourDiffFeatures = [
+  {
+    icon: "/service/Frame1.png",
+    title: "App Application Development",
+    des: "Crafting intuitive, high-performance mobile apps that elevate user engagement and drive business growth.",
+  },
+  {
+    icon: "/service/Frame2.png",
+    title: "Blockchain Development",
+    des: "Building secure, decentralized blockchain solutions that empower transparent and reliable transactions.",
+  },
+  {
+    icon: "/service/Frame3.png",
+    title: "Cloud Services",
+    des: "Providing robust, flexible cloud solutions to streamline operations and boost scalability.",
+  },
+  {
+    icon: "/service/Frame4.png",
+    title: "Web Application Development",
+    des: "Delivering efficient, scalable web applications tailored to meet your unique business needs.",
+  },
+];
+
+export const hitFeatures = [
+  {
+    icon: "/howItWorks/frame0-(2).png",
+    title: "Design",
+    des: "Creating visually engaging, user-centered designs aligned with your brand’s vision.",
+  },
+  {
+    icon: "/howItWorks/frame0-(1).png",
+    title: "Develop",
+    des: "Building scalable digital solutions with precision and performance at their core.",
+  },
+  {
+    icon: "/howItWorks/frame0-(3).png",
+    title: "Maintain",
+    des: "Keeping your products secure, updated, and optimized for continuous growth.",
+  },
+];
+
+export const whoWeInvest = [
+  {
+    title: "Respect",
+    des: "We build lasting relationships by fostering a culture of trust, integrity, and mutual respect.",
+  },
+  {
+    title: "Innovation",
+    des: "Embracing creativity and forward-thinking to deliver solutions that drive meaningful change.",
+  },
+  {
+    title: "Teamwork",
+    des: "Working collaboratively, combining strengths to achieve outstanding results for our clients.",
+  },
+  {
+    title: "Excellence",
+    des: "Committed to delivering quality and exceeding expectations in every project we undertake.",
+  },
+];
+
+export const testimonialsData = [
+  {
+    comment:
+      "SUPER HAPPY! Not only do you get to see the best seed and Early Stage companies but the exciting part is there’s a bunch of investors",
+    name: "Zach Lee",
+    profession: "QA specialist, Matrix architect",
+  },
+  {
+    comment:
+      "I have been working with Cotek on our fundraising strategies. They understands the in depth strategies of Venture Capital and early stage fund raising/company",
+    name: "Cheryl R. Weiss",
+    profession: "Founder, Dubrow Cafeteria",
+  },
+  {
+    comment:
+      "Another masterpiece really digging into the art of venture funds, portfolio construction and deal mechanics! Loved it - I can say without hesitation that it is incredibly valuable for anyone who is investing",
+    name: "Vicky R. Lane",
+    profession: "Directork, Lafayette Radio",
+  },
+  {
+    comment:
+      "BlueVenture is the premier setting for entrepreneurs and investors alike to exchange ideas, and network. As an entrepreneur, I can rely on the BlueVenture to bring top caliber speakers, entrepreneurs and investors",
+    name: "Jennifer Dubois",
+    profession: "Manager, China Coast",
+  },
+];
