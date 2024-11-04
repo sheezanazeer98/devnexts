e4476611a03e9f4d8e1967d29b7c815ca963fcb7
Sheeza Nazeer <sheezanazeer98@gmail.com>
2024-11-04 17:24:49 +0500

    envs

--- pages/api/collectEmail.js
+++ pages/api/collectEmail.js
@@ -7,32 +7,46 @@ export default async function handler(req, res) {
 
     try {
 
-    //   // Authenticate with Google Sheets
-    //   const auth = new google.auth.GoogleAuth({
-    //     credentials: JSON.parse(process.env.GOOGLE_SERVICE_ACCOUNT_CREDENTIALS),       
-    //     scopes: 'https://www.googleapis.com/auth/spreadsheets',
-    //   });
+      console.log(process.env.GOOGLE_TYPE);
+
+      // Authenticate with Google Sheets
+      const auth = new google.auth.GoogleAuth({
+        credentials:{
+          type: process.env.GOOGLE_TYPE,
+          project_id: process.env.GOOGLE_PROJECT_ID,
+          private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
+          private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+          client_email: process.env.GOOGLE_CLIENT_EMAIL,
+          client_id: process.env.GOOGLE_CLIENT_ID,
+          auth_uri: process.env.GOOGLE_AUTH_URI,
+          token_uri: process.env.GOOGLE_TOKEN_URI,
+          auth_provider_x509_cert_url: process.env.GOOGLE_AUTH_PROVIDER_CERT_URL,
+          client_x509_cert_url: process.env.GOOGLE_CLIENT_CERT_URL,
+          universe_domain: process.env.GOOGLE_UNIVERSE_DOMAIN,
+        },    
+        scopes: 'https://www.googleapis.com/auth/spreadsheets',
+      });
 
 
-    //   const sheets = google.sheets({ version: 'v4', auth });
+      const sheets = google.sheets({ version: 'v4', auth });
 
-    //   const spreadsheetId = '1-07Xzz-tdUtTIGfd3mTGJszOBnGpz_TWIShWgTWVQPE';
-    //   const range = 'Sheet1!A:B'; 
+      const spreadsheetId = '1-07Xzz-tdUtTIGfd3mTGJszOBnGpz_TWIShWgTWVQPE';
+      const range = 'Sheet1!A:B'; 
    
-    //   // Appending email and timestamp to the sheet
-    //  await sheets.spreadsheets.values.append({
-    //     spreadsheetId,
-    //     range,
-    //     valueInputOption: 'USER_ENTERED',
-    //     requestBody: {
-    //       values: [
-    //         [email, new Date().toLocaleString()], 
-    //       ],
-    //     },
-    //   });
+      // Appending email and timestamp to the sheet
+     await sheets.spreadsheets.values.append({
+        spreadsheetId,
+        range,
+        valueInputOption: 'USER_ENTERED',
+        requestBody: {
+          values: [
+            [email, new Date().toLocaleString()], 
+          ],
+        },
+      });
 
    
-    //   console.log("send");
+      console.log("send");
 
       res.status(200).json({ message: 'Email collected successfully!' });
     } catch (error) {
