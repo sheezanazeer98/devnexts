5720ee3de3e256c020c69300f3b7ebedf7b2940c
sheezanazeer98 <sheezanazeer98@gmail.com>
2026-04-09 20:09:11 +0500

    fiximages

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    images: {
+      unoptimized: true,
+    },
     env: {
       GOOGLE_SERVICE_ACCOUNT_CREDENTIALS: process.env.GOOGLE_SERVICE_ACCOUNT_CREDENTIALS,
       GOOGLE_SHEET_ID: process.env.GOOGLE_SHEET_ID,
